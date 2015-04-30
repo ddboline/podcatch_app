5d428726b7130e802e2cb5d372b59aa8e1d61729
Daniel Boline <daniel.boline@stonybrook.edu>
2015-04-30 10:29:25 -0400

    ...

--- podcatch.py
+++ podcatch.py
@@ -3,12 +3,17 @@
 '''
     podcatching app
 '''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import os
 
 from podcatch_app.podcatch_class import Podcasts, Episodes
 from podcatch_app.sqlite_dump import connect_sqlite, dump_sqlite_memory
 
-from urllib2 import urlopen
+from podcatch_app.util import openurl, get_md5
 
 import lxml.etree
 
@@ -17,46 +22,43 @@ OUTPUT_DIRECTORIES = {
     23: '/home/ddboline/Documents/podcasts/The_Bugle/',
     24: '/home/ddboline/Documents/podcasts/Welcome_to_Night_Vale/',}
 
-def add_podcast():
+def add_podcast(cid=-1, cname='', furl=''):
+    if cid == -1 and not cname and not furl:
+        return
     _con = connect_sqlite()
     podcasts = dump_sqlite_memory(sqlite_con=_con, dumpclass=Podcasts)
     episodes = dump_sqlite_memory(sqlite_con=_con, dumpclass=Episodes)
 
     for pod in podcasts:
-        print pod
-        print OUTPUT_DIRECTORIES[pod.castid]
-    #pod = Podcasts(castid=24, castname=u'Welcome to Night Vale', feedurl=u'http://nightvale.libsyn.com/rss', pcenabled=1, lastupdate=0, lastattempt=0, failedattempts=0)
-    #_con.execute(pod.sql_insert_string())
+        print(pod)
+        print(OUTPUT_DIRECTORIES[pod.castid])
+
+    for ep in episodes:
+        if cid != ep.castid:
+            continue
+        if furl == ep.epurl:
+            return
+
+    pod = Podcasts(castid=24, castname=u'Welcome to Night Vale',
+                   feedurl=u'http://nightvale.libsyn.com/rss',
+                   pcenabled=1, lastupdate=0, lastattempt=0, failedattempts=0)
+    _con.execute(pod.sql_insert_string())
 
 def podcatch(args):
     _con = connect_sqlite()
     podcasts = dump_sqlite_memory(sqlite_con=_con, dumpclass=Podcasts)
     episodes = dump_sqlite_memory(sqlite_con=_con, dumpclass=Episodes)
 
-    #_con.execute('DELETE FROM episodes WHERE title="1 - Pilot";')
-    #_con.execute('UPDATE episodes SET status="Unknown" WHERE episodeid=1876;')
-
     cur_urls = {}
     epids = []
     for ep in episodes:
         cur_urls[ep.epurl] = ep
-        #try:
-            #print ep
-        #except UnicodeEncodeError:
-            #print ep.__repr__().encode(errors='ignore')
-        if ep.episodeid not in epids:
-            epids.append(ep.episodeid)
-    #exit(0)
-    urls_to_download = []
+        epids.append(ep.episodeid)
     purls = []
-    curtitle = ''
-    newepid = sorted(epids)[-1]
+    newepid = sorted(set(epids))[-1]
     for p in podcasts:
-        _url = urlopen(p.feedurl)
+        _url = openurl(p.feedurl)
         _pep = Episodes()
-        #for line in _url:
-            #print line.strip()
-        #exit(0)
         for line in lxml.etree.parse(_url).iter():
             if line.tag == 'title':
                 if _pep.epurl:
@@ -89,27 +91,25 @@ def podcatch(args):
                 purls.append(_pep)
 
     for ep in purls:
-        print ep.sql_insert_string()
+        print(ep.sql_insert_string())
         FNAME = os.path.basename(ep.epurl)
-        #os.system('wget %s' % ep.epurl)
 
         with open(FNAME, 'wb') as outfile:
-            urlout = urlopen(ep.epurl)
+            urlout = openurl(ep.epurl)
             if urlout.getcode() != 200:
                 print('something bad happened %d' % urlout.getcode())
                 exit(0)
             for line in urlout:
                 outfile.write(line)
-
-        print os.stat(FNAME)
+        ep.epguid = get_md5(FNAME)
+        print(os.stat(FNAME))
         if os.stat(FNAME).st_size > 0:
             os.system('mv %s %s' % (FNAME, OUTPUT_DIRECTORIES[ep.castid]))
             ep.status = u'Downloaded'
             ep.epfailedattempts = 0
+            from sqlalchemy.exc import IntegrityError
             _con.execute(ep.sql_insert_string())
-
     return
 
 if __name__ == '__main__':
-    #add_podcast()
     podcatch(os.sys.argv)
