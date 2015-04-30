eeff39ad6de9cc57d1730effd9c8e71a787a457e
Daniel Boline <daniel.boline@stonybrook.edu>
2015-04-30 10:31:19 -0400

    ...

--- podcatch_app/sqlite_dump.py
+++ podcatch_app/sqlite_dump.py
@@ -2,8 +2,11 @@
 '''
     Dump sqlite db
 '''
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 
-import os
 from sqlalchemy import create_engine
 
 def connect_sqlite():
@@ -22,7 +25,7 @@ def dump_sqlite_memory(sqlite_con=None, dumpclass=None):
     for line in dbcon.execute(sqlite_query):
         classinst = dumpclass()
         if len(line) != len(dumpclass.__columns__):
-            print 'something is not right'
+            print('something is not right')
         for n in range(len(line)):
             setattr(classinst, dumpclass.__columns__[n], line[n])
         outlist.append(classinst)
